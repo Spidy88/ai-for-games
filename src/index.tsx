d804d26a86b47a9489f46b681c6377404d178367
Nick Ferraro <Spidy88@users.noreply.github.com>
2021-06-18 16:44:47 -0700

    Change router base path to use env vars

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import { Router } from "@reach/router"
 import './index.css';
 import { LandingPage } from './pages';
 
+const basePath = process.env.BASE_PATH ?? '';
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basepath={basePath}>
       <LandingPage path="/" />
     </Router>
   </React.StrictMode>,
