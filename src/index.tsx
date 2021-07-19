c19dbc230605d3a9976c8fad4c205d1b823d7c75
Nick Ferraro <spidy.88@gmail.com>
2021-07-19 01:17:21 -0700

    Getting there

--- src/index.tsx
+++ src/index.tsx
@@ -4,51 +4,91 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useRouteMatch
 } from 'react-router-dom';
 //import './index.css';
 import { LandingPage, ExplorePage, PlaygroundPage, AlgorithmPage } from './pages';
-import homeIcon from './assets/icons/home.svg';
-import exploreIcon from './assets/icons/explore.svg';
-import playgroundIcon from './assets/icons/playground.svg';
 import twitchLogo from './assets/icons/twitch-logo.svg';
+
+function Sidebar() {
+  return (
+    <div className="sidebar" data-color="white" data-active-color="danger">
+      <div className="logo">
+        <div className="simple-text logo-normal text-center" style={{ padding: '11px 0 8px' }}>
+          AI for Games
+        </div>
+      </div>
+
+      <div className="sidebar-wrapper">
+        <ul className="nav">
+          <NavItem to="/" label="Home" icon="nc-bank" />
+          <NavItem to="/explore" label="Explore" icon="nc-diamond" />
+          <NavItem to="/playground" label="Playground" icon="nc-pin-3" />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+type NavItemProps = {
+  to: string;
+  label: string;
+  icon: string;
+};
+function NavItem(props: NavItemProps) {
+  const { to, label, icon } = props;
+  const match = useRouteMatch({
+    path: to,
+    exact: true,
+    strict: true,
+    sensitive: true
+  });
+  const activeClass = match ? 'active' : '';
+
+  return (
+    <li className={`overflow-hidden ${activeClass}`}>
+      <Link to={to}>
+        <i className={`nc-icon ${icon}`}></i>
+        <p>{label}</p>
+      </Link>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer" style={{ position: 'absolute', bottom: 0, width: '-webkit-fill-available' }}>
+      <div className="container-fluid">
+        <div className="row">
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a href="https://twitch.tv/Spidy88" target="_blank" rel="noreferrer">
+                  <img src={twitchLogo} alt="twitch logo" aria-hidden={true} style={{ marginRight: '0.5rem', height: '1rem' }} />Catch me on Twitch
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="d-sm-flex d-md-none" style={{ flexBasis: '100%' }} />
+
+          <div className="credits ml-auto">
+            <span className="copyright">
+              © 2020, Nick Ferraro
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <div className="wrapper ">
-        <div className="sidebar" data-color="white" data-active-color="danger">
-          <div className="logo">
-            <div className="simple-text logo-normal text-center" style={{ padding: '11px 0 8px' }}>
-              AI for Games
-            </div>
-          </div>
-
-          <div className="sidebar-wrapper">
-            <ul className="nav">
-              <li className="active ">
-                <Link to="/">
-                  <i className="nc-icon nc-bank"></i>
-                  <p>Home</p>
-                </Link>
-              </li>
-
-              <li>
-                <Link to="/explore">
-                  <i className="nc-icon nc-diamond"></i>
-                  <p>Explore</p>
-                </Link>
-              </li>
-              
-              <li>
-                <Link to="/playground">
-                  <i className="nc-icon nc-pin-3"></i>
-                  <p>Playground</p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Sidebar />
 
         <div className="main-panel" style={{ height: '100vh' }}>
           <nav className="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
@@ -68,12 +108,16 @@ ReactDOM.render(
 
           <div className="content">
             <Switch>
-              <Route exact path="/">
+              <Route exact path="/extra">
                 <div className="row">
                   <div className="col-md-12">
                     <h3 className="description">Your content here</h3>
                   </div>
                 </div>
+              </Route>
+
+              <Route exact path="/">
+                <LandingPage />
               </Route>
 
               <Route exact path="/explore">
@@ -90,75 +134,10 @@ ReactDOM.render(
             </Switch>
           </div>
 
-          <footer className="footer" style={{ position: 'absolute', bottom: 0, width: '-webkit-fill-available' }}>
-            <div className="container-fluid">
-              <div className="row">
-                <nav className="footer-nav">
-                  <ul>
-                    <li>
-                      <a href="https://twitch.tv/Spidy88" target="_blank" rel="noreferrer">
-                        <img src={twitchLogo} alt="twitch logo" aria-hidden={true} style={{ marginRight: '0.5rem', height: '1rem' }} />Catch me on Twitch
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-
-                <div className="d-sm-flex d-md-none" style={{ flexBasis: '100%' }} />
-
-                <div className="credits ml-auto">
-                  <span className="copyright">
-                    © 2020, Nick Ferraro
-                  </span>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <Footer />
         </div>
       </div>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <LeftNavigation>
-        <NavLink to="/">
-          <NavIcon src={homeIcon} alt="Go to Home page" />
-          Home
-        </NavLink>
-
-        <NavLink to="/explore">
-          <NavIcon src={exploreIcon} alt="Go to Explore page" />
-          Explore
-        </NavLink>
-
-        <NavLink to="/playground">
-          <NavIcon src={playgroundIcon} alt="Go to Playground page" />
-          Playground
-        </NavLink>
-      </LeftNavigation>
-
-      <Content>
-        <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route exact path="/explore">
-            <ExplorePage />
-          </Route>
-          <Route path="/explore/:algorithm">
-            <AlgorithmPage />
-          </Route>
-          <Route exact path="/playground">
-            <PlaygroundPage />
-          </Route>
-        </Switch>
-      </Content>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-*/
